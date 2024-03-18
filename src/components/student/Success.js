df175c9a3aa2dc3df56ecb36e09c36c561a2382f
Grace Yan <xgraceyan@gmail.com>
2024-03-18 15:09:52 -0700

    Update Success.js

--- src/components/student/Success.js
+++ src/components/student/Success.js
@@ -5,10 +5,13 @@ const Success = () => {
     <section id="student-page">
       <div className="container-lg">
         <h1 className="title text-center">Ask a Cougar</h1>
-        <h6 className="text-center text-label-box">
+        <h4 className="text-center text-label-box">
           Thank you for submitting your response! We appreciate you helping our
           service project out. Now relax and enjoy the rest of homeroom!
-        </h6>
+        </h4>
+        <h4 className="text-center text-label-box">
+          <a href="">Have extra time? Answer another question.</a>
+        </h4>
       </div>
     </section>
   );
